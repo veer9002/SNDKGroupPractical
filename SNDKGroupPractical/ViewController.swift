f79c67efbada85d90513e2139fc9a0aa432ff0d2
Your Name <mann_9898771157@yahoo.com>
2021-03-21 06:34:03 +0530

    Initial Commit

---
+++ SNDKGroupPractical/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SNDKGroupPractical
+//
+//  Created by Manish Sharma on 21/03/21.
+//  Copyright © 2021 Manish Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
